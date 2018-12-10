7bf20ba3e58f2d6ae581a6d594026756023e7041
William Miranda <williamlopes.miranda@altran.com>
2018-12-10 18:33:21 +0000

    Adding stuff
    Signed-off-by: William Miranda <williamlopes.miranda@altran.com>

--- app/src/main/java/com/dummyapp/activities/BaseActivity.java
+++ app/src/main/java/com/dummyapp/activities/BaseActivity.java
@@ -2,32 +2,34 @@ package com.dummyapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-import com.dummyapp.applications.BaseApplication;
-import com.dummyapp.dagger.DaggerIBaseComponent;
-import com.framework.cache.services.CacheService;
-import com.framework.domain.doctors.Doctor;
+import com.dummyapp.R;
 
 import javax.inject.Inject;
 
-/***
- * This class aims to be the base activity which will be inherited by each activity in the project and
- * provide the common methods which should be consumed by each of the available activities
- */
-public abstract class BaseActivity extends AppCompatActivity {
+import three.appbase.turbuplus.astrazeneca.com.turbuplusfwk.appevent.commands.AppEventPushCommand;
+import three.appbase.turbuplus.astrazeneca.com.turbuplusfwk.core.dispatchers.CommandsDispatcher;
 
+public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Since we might need this on several Activities,
-     * we may declare all injections in a BaseActivity and have all others inherit from this
-     * So Dagger may correctly Inject the required dependencies for all the activities.
-     */
     @Inject
-    public CacheService<Doctor> doctorCacheService;
+    CommandsDispatcher commandsDispatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ((BaseApplication) getApplication()).getBaseComponent().inject(this);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Cria um comando a ser enviado para o Dispatcher
+                AppEventPushCommand pushCommand = new AppEventPushCommand();
+
+                //Envia o comando
+                commandsDispatcher.process(pushCommand);
+            }
+        });
     }
 }
